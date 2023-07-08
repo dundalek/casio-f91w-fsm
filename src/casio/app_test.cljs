32d24b92414bfdd3f754452996f9d29d49375594
Jakub Dundalek <dundalek@gmail.com>
2023-07-08 12:33:26 +0200

    wip test tweaks

--- src/casio/app_test.cljs
+++ src/casio/app_test.cljs
@@ -4,46 +4,56 @@
    [clojure.test :refer [deftest is]]
    ["xstate" :as xstate]))
 
+(defn send [actor event]
+  (assert (keyword? event))
+  (.send actor #js {:type (name event)}))
+
 (defn press-c-button [actor]
-  (.send actor #js {:type "c-down"})
-  (.send actor #js {:type "c-up"}))
+  (send actor :c-down)
+  (send actor :c-up))
 
 (defn press-a-button [actor]
-  (.send actor #js {:type "a-down"})
-  (.send actor #js {:type "a-up"}))
+  (send actor :a-down)
+  (send actor :a-up))
 
 (defn state-value [^Actor actor]
-  (js/console.log "actor")
   (js->clj (.-value (.getSnapshot actor))
            :keywordize-keys true))
 
-(defn ^js get-context [^Actor actor]
-  (.-context (.getSnapshot actor)))
+(defn get-context [^Actor actor]
+  (js->clj (.-context (.getSnapshot actor))
+           :keywordize-keys true))
 
 (defn make-watch-actor []
   (let [actor (xstate/interpret watch-machine)]
     (.start actor)
     actor))
 
-(deftest cycle-through-modes
+(defn test-machine [f]
   (let [actor (make-watch-actor)]
-    (is (= {:dateTime "default"} (state-value actor)))
-    (press-c-button actor)
-    (is (= {:dailyAlarm "default"} (state-value actor)))
-    (press-c-button actor)
-    (is (= {:stopwatch "default"} (state-value actor)))
-    (press-c-button actor)
-    (is (= {:setDateTime "default"} (state-value actor)))
-    (press-c-button actor)
-    (is (= {:dateTime "default"} (state-value actor)))))
+    (f actor)))
+
+(deftest cycle-through-modes
+  (test-machine
+   (fn [actor]
+     (is (= {:dateTime "default"} (:watch (state-value actor))))
+     (press-c-button actor)
+     (is (= {:dailyAlarm "default"} (:watch (state-value actor))))
+     (press-c-button actor)
+     (is (= {:stopwatch "default"} (:watch (state-value actor))))
+     (press-c-button actor)
+     (is (= {:setDateTime "default"} (:watch (state-value actor))))
+     (press-c-button actor)
+     (is (= {:dateTime "default"} (:watch (state-value actor)))))))
 
 (deftest toggle-time-mode
-  (let [actor (make-watch-actor)]
-    (is (= "24" (.-timeMode (get-context actor))))
-    (press-a-button actor)
-    (is (= "12" (.-timeMode (get-context actor))))
-    (press-a-button actor)
-    (is (= "24" (.-timeMode (get-context actor))))))
+  (test-machine
+   (fn [actor]
+     (is (= "24" (:timeMode (get-context actor))))
+     (press-a-button actor)
+     (is (= "12" (:timeMode (get-context actor))))
+     (press-a-button actor)
+     (is (= "24" (:timeMode (get-context actor)))))))
 
 #_(deftest toggle-casio-screen)
 
@@ -58,19 +68,19 @@
       alarmOnMark timeSignalOnMark))
 
 (deftest set-alarm
-  (let [actor (make-watch-actor)]
-    (.send actor #js {:type "c-down"})
-    (.send actor #js {:type "l-down"})
-    (.send actor #js {:type "a-down"})
-    (.send actor #js {:type "a-down"})
-    (.send actor #js {:type "l-down"})
-    (.send actor #js {:type "a-down"})
-    (.send actor #js {:type "a-down"})
-    (.send actor #js {:type "a-down"})
-    (.send actor #js {:type "l-down"})
-    (let [context (get-context actor)
-          dailyAlarmDateTime (.-dailyAlarmDateTime context)]
-      (is (= {:dailyAlarm "default"} (state-value actor)))
-      (is (= true (.-alarmOnMark context)))
-      (is (= 9 (.getHours dailyAlarmDateTime)))
-      (is (= 3 (.getMinutes dailyAlarmDateTime))))))
+  (test-machine
+   (fn [actor]
+     (send actor :c-down)
+     (send actor :l-down)
+     (send actor :a-down)
+     (send actor :a-down)
+     (send actor :l-down)
+     (send actor :a-down)
+     (send actor :a-down)
+     (send actor :a-down)
+     (send actor :l-down)
+     (let [{:keys [dailyAlarmDateTime alarmOnMark]} (get-context actor)]
+       (is (= {:dailyAlarm "modified"} (:watch (state-value actor))))
+       (is (= true alarmOnMark))
+       (is (= 9 (.getHours dailyAlarmDateTime)))
+       (is (= 3 (.getMinutes dailyAlarmDateTime)))))))
